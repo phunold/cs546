bcea6adb53ad205eea812ac67aae6794b7982fdd
troy <troykoss@gmail.com>
2016-07-27 23:07:52 -0400

    Minor update. Forgot to save file before commit

--- API/DAL/index.js
+++ API/DAL/index.js
@@ -1,13 +1,4 @@
-// const recipeRoutes = require("./recipes");
-
-// let constructorMethod = (app) => {
-//     app.use("/recipes", recipeRoutes);
-//     app.use("/recipes", recipeRoutes);
-
-// };
-
 module.exports = {
-    recipeDAL: require("./recipes"),
-    recipeCommentsDAL: require("./recipeComments")
+    exampleDAL: require("./example"),
 
 };
