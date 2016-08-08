8557870b241ff35206f48be2c479543098991b31
Brian Kelly <lemonadecollege@gmail.com>
2016-08-08 18:13:24 -0400

    added GET functions for users

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,14 +1,23 @@
 const express = require('express');
 const router = express.Router();
 const DAL = require("../DAL");
-const exampleDAL = DAL.usersDAL;
+const usersDAL = DAL.usersDAL;
 //const Data = require("../Data");
 //const exampleData = Data.exampleData;
 
 //GET METHODS
-//
-router.get("/", (req, res) => { 
-    exampleDAL.exampleDBCall().then((dataList) => {
+//Get by ID
+router.get("/:id", (req, res) => { 
+    usersDAL.getUserByID().then((dataList) => {
+        res.json(dataList);
+    }).catch((e) => {
+        res.status(500).json({ error: e });
+    });
+});
+
+//Get by Email
+router.get("/:email", (req, res) => { 
+    usersDAL.getUserByEmail().then((dataList) => {
         res.json(dataList);
     }).catch((e) => {
         res.status(500).json({ error: e });
