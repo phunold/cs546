1f2a48251645ea1093e0d575b0072958b38a0888
Brian Kelly <lemonadecollege@gmail.com>
2016-08-14 10:16:21 -0400

    stubbed out data/auth.js

--- src/data/auth.js
+++ src/data/auth.js
@@ -1,4 +1,27 @@
+const uuid = require('node-uuid');
+const bcrypt = require('bcrypt-nodejs');
+const DAL = require("../DAL");
+const usersDAL = DAL.usersDAL;
 // needs a loginUser(email,password) func to login the user, create a SESSION_ID and USER_ID cookie. returns the sessionid (a uuid);
 // needs a terminateSession(sessionId,userId) func to remove that session ID from the user's sessions array in mongoDB
+let exportedMethods = {
+	
+	loginUser(email, password){
+		var user = usersDAL.getUserByEmail(email);
+		bcrypt.compare(password, user.passwd, function(err, res){
+			if(res === true){
+				//Successful authentication
+				var SESSION_ID = uuid.v1();
+				//Store cookie
+			}else{
+				//Incorrect password
+			}
+		});
+		return SESSION_ID;
+	},
 
-
+	terminateSession(sessionId, userId){
+		var user = usersDAL.getUserByID(userId);
+		//Remove cookies
+	}
+}
