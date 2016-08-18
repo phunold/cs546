f94eb17efb457e962322f8f28579a93bbd332d1b
Ezhilarasi Swaminathan <ezhilarasi.swaminathan@gmail.com>
2016-08-18 19:54:13 -0400

    Scores DAL

--- src/DAL/scores.js
+++ src/DAL/scores.js
@@ -3,31 +3,31 @@ const mongoCollections = require("../config/mongoCollections");
 const scoresCollection = mongoCollections.scores;
 
 const uuid = require('node-uuid');
-const ObjectId = require('mongodb').ObjectId; 
+const ObjectId = require('mongodb').ObjectId;
 
 let exportedMethods = {
 
-    getScores(){
-        return scoresCollection().then((scores)=>{
+    getScores() {
+        return scoresCollection().then((scores) => {
             console.log("Got scores!");
             return scores;
-        }, (error)=>{
+        }, (error) => {
             throw "Couldn't get scores!";
         })
-		
-		
+
+
     },
-	
-	insertMany(scoresParam) {
-		return scoresCollection().then((scores)=>{
-            return scores.insertMany(scoresParam).then((response)=>{
-                if(response.acknowledged) return response.insertedIds;
+
+    insertMany(scoresParam) {
+        return scoresCollection().then((scores) => {
+            return scores.insertMany(scoresParam).then((response) => {
+                if (response.acknowledged) return response.insertedIds;
                 else throw "Insertion error!";
             })
-        },(error)=>{
+        }, (error) => {
             throw "Couldn't retreive scores collection";
-        })     
-    }   		
+        })
+    }
 }
 
 module.exports = exportedMethods;
